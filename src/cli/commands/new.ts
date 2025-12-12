9e0c68235496ecb0c717b9197a1987c4a740868b
fabrisdev <ezepuntocom@hotmail.com>
2025-12-12 19:01:10 -0300

    Added dev script

--- src/cli/commands/new.ts
+++ src/cli/commands/new.ts
@@ -58,6 +58,8 @@ export default async (_params: RouteParams) => {
 </body>
 </html>`;
 	await fs.writeFile(path.join(fullPath, "views", "example.html"), viewContent);
+	console.log("5. Creating run scripts...");
+	await runCommand(`npm pkg set scripts.dev="bun index.ts"`);
 	console.log(`All done! Project ${name} created.`);
 	console.log(`Now run cd ${name} and start building your next big project!`);
 };
