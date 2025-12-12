d13c69217fc3944f857f3b0c1062855b9e98a406
fabrisdev <ezepuntocom@hotmail.com>
2025-12-12 20:42:45 -0300

    Added new command on help output

--- src/cli/consts.ts
+++ src/cli/consts.ts
@@ -1,2 +1,2 @@
 export const AVAILABLE_COMMANDS =
-	"1. htmv new {project_name} (creates a new HTMV project)\n2. htmv help (prints this page)";
+	"1. htmv new {project_name} (creates a new HTMV project)\n2. htmv help (prints this page)\n3. htmv gen {TYPE} {NAME} {OPTIONS} (generates files for you)";
