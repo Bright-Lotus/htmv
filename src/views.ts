63c711fd2a8202c2ac05d2cce869522aca972fa3
fabrisdev <ezepuntocom@hotmail.com>
2025-12-15 18:13:42 -0300

    Added Isset

--- src/views.ts
+++ src/views.ts
@@ -14,9 +14,18 @@ export async function view(view: string, props: Record<string, unknown>) {
 		);
 	const filePath = path.join(viewsPath, `${view}.html`);
 	const code = await fs.readFile(filePath, "utf-8");
-	const replacedCode = code.replace(/{(.+)}/g, (_, propName) => {
-		return props[propName] as string;
-	});
+	const replacedCode = code
+		.replace(/{(.+)}/g, (_, propName) => {
+			return props[propName] as string;
+		})
+		.replace(
+			/<Isset\s+(\w+)>([\s\S]*?)<\/Isset>/g,
+			(_, propName, innerContent) => {
+				if (props[propName] !== undefined && props[propName] !== null)
+					return innerContent;
+				return "";
+			},
+		);
 	return new Response(replacedCode, {
 		headers: { "Content-Type": "text/html; charset=utf-8" },
 	});
