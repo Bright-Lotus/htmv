6f58ef5114b431f35b2fced0bce7ec4f8a8ca90a
fabrisdev <ezepuntocom@hotmail.com>
2025-12-15 20:35:10 -0300

    Added for

--- src/views.ts
+++ src/views.ts
@@ -15,6 +15,25 @@ export async function view(view: string, props: Record<string, unknown>) {
 	const filePath = path.join(viewsPath, `${view}.html`);
 	const code = await fs.readFile(filePath, "utf-8");
 	const replacedCode = code
+		.replace(
+			/<For\s+(\w+)\s+in\s+(\w+)>([\s\S]*?)<\/For>/g,
+			(_, itemName: string, listName: string, innerContent: string) => {
+				const list = props[listName];
+				if (!Array.isArray(list))
+					throw new Error(
+						`${listName} on view ${view} is not an array. If you wish for the For to not do anything pass an empty array instead.`,
+					);
+
+				return list
+					.map((item) =>
+						innerContent.replace(
+							new RegExp(`{${itemName}}`, "g"),
+							String(item),
+						),
+					)
+					.join("");
+			},
+		)
 		.replace(/{(\w+)}/g, (_, propName) => {
 			return props[propName] as string;
 		})
