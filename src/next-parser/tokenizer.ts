cda59b13dfaec286e9bd4cb9c722076ff5449b1b
fabrisdev <ezepuntocom@hotmail.com>
2025-12-16 20:06:19 -0300

    Added InterpolationToken

--- src/next-parser/tokenizer.ts
+++ src/next-parser/tokenizer.ts
@@ -1,6 +1,11 @@
-type Token = TextToken;
+type Token = TextToken | InterpolationToken;
 
 type TextToken = {
 	type: "text";
 	text: string;
+};
+
+type InterpolationToken = {
+	type: "interpolation";
+	value: string;
 };
