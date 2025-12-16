7571c03b84b3460ea13e1b65c230c4253eea67cd
fabrisdev <ezepuntocom@hotmail.com>
2025-12-16 20:03:41 -0300

    Implemented renderer

--- src/next-parser/renderer.ts
+++ src/next-parser/renderer.ts
@@ -28,4 +28,36 @@ interface ForNode {
 	children: Node[];
 }
 
-function render(node: Node, context: Record<string, unknown>): string {}
+function render(node: Node, context: Record<string, unknown>): string {
+	if (node.type === "text") {
+		return node.text;
+	}
+	if (node.type === "for") {
+		const list = context[node.listName];
+		if (Array.isArray(list)) {
+			const output = list.map((item) => {
+				return node.children
+					.map((childrenNode) =>
+						render(childrenNode, { ...context, [node.itemName]: item }),
+					)
+					.join("");
+			});
+			return output.join("");
+		}
+		throw new Error("La lista pasada no es un array");
+	}
+	if (node.type === "interpolation") {
+		return String(context[node.value]);
+	}
+	if (node.type === "isset") {
+		if (
+			context[node.itemName] !== undefined &&
+			context[node.itemName] !== null
+		) {
+			return node.children.map((node) => render(node, context)).join("");
+		}
+		return "";
+	}
+	const output = node.children.map((node) => render(node, context));
+	return output.join("");
+}
