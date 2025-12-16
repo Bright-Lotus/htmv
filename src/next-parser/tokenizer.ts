07c0832364e68600503a8bbdf700b2c69530f1c7
fabrisdev <ezepuntocom@hotmail.com>
2025-12-16 20:08:06 -0300

    Added helper function

--- src/next-parser/tokenizer.ts
+++ src/next-parser/tokenizer.ts
@@ -31,3 +31,28 @@ type ArgumentsToken = {
 };
 
 function tokenize(input: string): Token[] {}
+
+function readTill(
+	charToSearchFor: string,
+	startingPosition: number,
+	text: string,
+):
+	| {
+			success: true;
+			value: string;
+			finishingPosition: number;
+	  }
+	| {
+			success: false;
+	  } {
+	const finishingPosition = text.indexOf(charToSearchFor, startingPosition + 1);
+	if (finishingPosition === -1) {
+		return { success: false };
+	}
+	const substring = text.substring(startingPosition + 1, finishingPosition);
+	return {
+		success: true,
+		value: substring,
+		finishingPosition,
+	};
+}
