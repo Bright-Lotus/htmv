8082f18bed7a1099d420d2c4183b29903eba1957
fabrisdev <ezepuntocom@hotmail.com>
2025-12-16 20:08:25 -0300

    Implemented tokenize function

--- src/next-parser/tokenizer.ts
+++ src/next-parser/tokenizer.ts
@@ -30,7 +30,72 @@ type ArgumentsToken = {
 	value: string[];
 };
 
-function tokenize(input: string): Token[] {}
+function tokenize(input: string): Token[] {
+	const tokens: Token[] = [];
+	let textBuffer = "";
+	for (let i = 0; i < input.length; i++) {
+		const char = input[i];
+		if (char === "{") {
+			pushTextBuffer();
+			const read = readTill("}", i, input);
+			if (!read.success) throw new Error("Unable to find closing }");
+			const { value, finishingPosition } = read;
+			tokens.push({
+				type: "interpolation",
+				value,
+			});
+			i = finishingPosition;
+			continue;
+		}
+
+		if (char === "<") {
+			pushTextBuffer();
+			const read = readTill(">", i, input);
+			if (!read.success) throw new Error("Unable to find closing '>'");
+			const { value, finishingPosition } = read;
+			const values = value.trim().split(/\s+/);
+			const tag = values[0];
+			if (tag === undefined) throw new Error("Unable to find tag name");
+			if (tag[0] === "/") {
+				const tagName = tag.substring(1);
+				tokens.push({
+					type: "close",
+					tag: tagName,
+				});
+				i = finishingPosition;
+				continue;
+			}
+			const args = values.slice(1);
+			tokens.push({
+				type: "open",
+				tag,
+			});
+			if (args.length > 0) {
+				tokens.push({
+					type: "arguments",
+					value: args,
+				});
+			}
+
+			i = finishingPosition;
+			continue;
+		}
+
+		textBuffer += char;
+	}
+	pushTextBuffer();
+	return tokens;
+
+	function pushTextBuffer() {
+		if (textBuffer.length > 0) {
+			tokens.push({
+				type: "text",
+				text: textBuffer,
+			});
+			textBuffer = "";
+		}
+	}
+}
 
 function readTill(
 	charToSearchFor: string,
