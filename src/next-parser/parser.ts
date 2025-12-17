d29f018fec898ab1699b8376cce1eddc5cbff117
fabrisdev <ezepuntocom@hotmail.com>
2025-12-16 21:40:09 -0300

    Implemented parser

--- src/next-parser/parser.ts
+++ src/next-parser/parser.ts
@@ -3,7 +3,7 @@ import type { Node, RootNode } from "./renderer";
 import { type Token, tokenize } from "./tokenizer";
 
 function parse(tokens: Token[]) {
-	const i = 0;
+	let i = 0;
 
 	const root: RootNode = {
 		type: "root",
@@ -12,7 +12,82 @@ function parse(tokens: Token[]) {
 
 	return root;
 
-	function parseChildren(untilTag?: string): Node[] {}
+	function parseChildren(untilTag?: string): Node[] {
+		const nodes: Node[] = [];
+		while (i < tokens.length) {
+			const token = tokens[i];
+			if (
+				untilTag &&
+				token?.type === "close" &&
+				token.tag.toLocaleLowerCase() === untilTag
+			) {
+				i++;
+				break;
+			}
+			if (token?.type === "text") {
+				nodes.push({
+					type: "text",
+					text: token.text,
+				});
+				i++;
+				continue;
+			}
+			if (token?.type === "interpolation") {
+				nodes.push({
+					type: "interpolation",
+					value: token.value,
+				});
+				i++;
+				continue;
+			}
+			if (token?.type === "open") {
+				const tag = token.tag.toLowerCase();
+				i++;
+				if (tag === "for") {
+					const nextToken = tokens[i]; //should be arguments token
+					i++;
+					if (nextToken?.type !== "arguments")
+						throw new Error("Missing arguments in for");
+					const [itemName, _in, listName] = nextToken.value;
+					if (
+						itemName === undefined ||
+						_in === undefined ||
+						listName === undefined
+					)
+						throw new Error(
+							"Incorrect amount of arguments in for. Expected 3 arguments",
+						);
+					if (_in !== "in") throw new Error("Expected reserved word in.");
+
+					const children = parseChildren(tag);
+					nodes.push({
+						type: "for",
+						children,
+						itemName,
+						listName,
+					});
+					continue;
+				}
+				if (tag === "isset") {
+					const nextToken = tokens[i]; //should be arguments token
+					i++;
+					if (nextToken?.type !== "arguments")
+						throw new Error("Missing arguments in isset");
+					const [itemName] = nextToken.value;
+					if (itemName === undefined) throw new Error("Expected item name");
+					const children = parseChildren(tag);
+					nodes.push({
+						type: "isset",
+						children,
+						itemName,
+					});
+					continue;
+				}
+				throw new Error(`Unknown tag <${tag}>`);
+			}
+		}
+		return nodes;
+	}
 }
 
 const input = await fs.readFile("test.html", "utf-8");
