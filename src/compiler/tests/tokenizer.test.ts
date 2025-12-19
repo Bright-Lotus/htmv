cc7a2d62fe11143010f609cc88cfadeb7cabd192
fabrisdev <ezepuntocom@hotmail.com>
2025-12-19 13:40:32 -0300

    Reading output

--- src/compiler/tests/tokenizer.test.ts
+++ src/compiler/tests/tokenizer.test.ts
@@ -1,14 +1,18 @@
 import { describe, expect, test } from "bun:test";
 import path from "node:path";
-import { type Token, tokenize } from "../tokenizer";
+import { tokenize } from "../tokenizer";
+
+const commonPath = ["src", "compiler", "tests", "tokenizer"];
 
 describe("Tokenizer tests", () => {
 	test("Simple HTML", async () => {
 		const input = await Bun.file(
-			path.resolve("src", "compiler", "tests", "tokenizer", "input", "1.html"),
+			path.resolve(...commonPath, "input", "1.html"),
 		).text();
 		const tokens = tokenize(input);
-		const expectedOutput: Token[] = [];
+		const expectedOutput = await Bun.file(
+			path.resolve(...commonPath, "output", "1.json"),
+		).json();
 		expect(tokens).toBe(expectedOutput);
 	});
 });
