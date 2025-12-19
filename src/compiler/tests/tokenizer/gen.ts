cf9c4cd0623edb956df6daf16561ca70e089ea53
fabrisdev <ezepuntocom@hotmail.com>
2025-12-19 15:40:28 -0300

    Added linting after file creation

--- src/compiler/tests/tokenizer/gen.ts
+++ src/compiler/tests/tokenizer/gen.ts
@@ -1,13 +1,17 @@
 import path from "node:path";
+import { $ } from "bun";
 import { tokenize } from "../../tokenizer";
 
 const fileName = process.argv[2];
 if (fileName === undefined) throw new Error("Missing input test file name");
 const input = await Bun.file(
-	path.resolve(import.meta.dir, "input", fileName),
+	path.resolve(import.meta.dir, "input", `${fileName}.html`),
 ).text();
 const tokens = tokenize(input);
-Bun.write(
-	path.resolve(import.meta.dir, "output", `${fileName}.json`),
-	JSON.stringify(tokens),
+const outputFilePath = path.resolve(
+	import.meta.dir,
+	"output",
+	`${fileName}.json`,
 );
+await Bun.write(outputFilePath, JSON.stringify(tokens));
+await $`bunx biome check ${outputFilePath} --fix`;
