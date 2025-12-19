1e6b4dbc6267f9065d41f3e6f745634c79feab81
fabrisdev <ezepuntocom@hotmail.com>
2025-12-19 14:15:33 -0300

    Working on test creation system

--- src/compiler/tests/tokenizer.test.ts
+++ src/compiler/tests/tokenizer.test.ts
@@ -1,18 +1,25 @@
 import { describe, expect, test } from "bun:test";
 import path from "node:path";
 import { tokenize } from "../tokenizer";
+import fs from "node:fs/promises";
 
 const commonPath = ["src", "compiler", "tests", "tokenizer"];
+const inputFilesPath = path.resolve(...commonPath, "input");
+const outputFilesPath = path.resolve(...commonPath, "output");
+const inputFilesNames = await fs.readdir(inputFilesPath);
 
 describe("Tokenizer tests", () => {
-	test("Simple HTML", async () => {
-		const input = await Bun.file(
-			path.resolve(...commonPath, "input", "1.html"),
-		).text();
-		const tokens = tokenize(input);
-		const expectedOutput = await Bun.file(
-			path.resolve(...commonPath, "output", "1.json"),
-		).json();
-		expect(tokens).toEqual(expectedOutput);
+	inputFilesNames.forEach((inputFileName) => {
+		const name = inputFileName.slice(0, -".html".length);
+		test(name, async () => {
+			const input = await Bun.file(
+				path.resolve(...inputFilesPath, inputFileName),
+			).text();
+			const tokens = tokenize(input);
+			const expectedOutput = await Bun.file(
+				path.resolve(...outputFilesPath, `${name}.json`),
+			).json();
+			expect(tokens).toEqual(expectedOutput);
+		});
 	});
 });
