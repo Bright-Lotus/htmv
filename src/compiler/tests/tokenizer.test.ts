49bd704b44f0cb01b120b3d8bb2c17adca47124e
fabrisdev <ezepuntocom@hotmail.com>
2025-12-19 13:36:43 -0300

    Implemented first test

--- src/compiler/tests/tokenizer.test.ts
+++ src/compiler/tests/tokenizer.test.ts
@@ -1,9 +1,12 @@
 import { describe, expect, test } from "bun:test";
+import path from "node:path";
 import { type Token, tokenize } from "../tokenizer";
 
 describe("Tokenizer tests", () => {
 	test("Simple HTML", async () => {
-		const input = await Bun.file("parser-1-input.html").text();
+		const input = await Bun.file(
+			path.resolve("src", "compiler", "tests", "tokenizer", "input", "1.html"),
+		).text();
 		const tokens = tokenize(input);
 		const expectedOutput: Token[] = [];
 		expect(tokens).toBe(expectedOutput);
