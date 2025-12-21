8dfbf5b62e990e03abab9e5c872e4e6ba379e095
fabrisdev <ezepuntocom@hotmail.com>
2025-12-21 19:50:46 -0300

    Adjusted to work with parser

--- src/compiler/tests/parser/parser.test.ts
+++ src/compiler/tests/parser/parser.test.ts
@@ -1,23 +1,23 @@
 import { describe, expect, test } from "bun:test";
 import fs from "node:fs/promises";
 import path from "node:path";
-import { render } from "../../renderer";
+import { parse } from "../../parser";
 
 const inputFilesPath = path.resolve(import.meta.dir, "input");
 const outputFilesPath = path.resolve(import.meta.dir, "output");
 const inputFilesNames = await fs.readdir(inputFilesPath);
 
-describe("Renderer tests", () => {
-  inputFilesNames.forEach((inputFileName) => {
-    const name = inputFileName.slice(0, -".ts".length);
-    test(name, async () => {
-      const input = (await import(path.resolve(inputFilesPath, inputFileName)))
-        .default;
-      const output = render(input.root, input.context);
-      const expectedOutput = await Bun.file(
-        path.resolve(outputFilesPath, `${name}.html`),
-      ).text();
-      expect(output).toEqual(expectedOutput);
-    });
-  });
+describe("Parser tests", () => {
+	inputFilesNames.forEach((inputFileName) => {
+		const name = inputFileName.slice(0, -".ts".length);
+		test(name, async () => {
+			const input = (await import(path.resolve(inputFilesPath, inputFileName)))
+				.default;
+			const output = parse(input);
+			const expectedOutput = await Bun.file(
+				path.resolve(outputFilesPath, `${name}.json`),
+			).json();
+			expect(output).toEqual(expectedOutput);
+		});
+	});
 });
