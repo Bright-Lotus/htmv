77990d16f07dd4f75f5e650059509576021e2460
fabrisdev <ezepuntocom@hotmail.com>
2025-12-21 19:58:38 -0300

    New test

---
+++ src/compiler/tests/parser/input/Simple text.ts
@@ -0,0 +1,20 @@
+import type { Token } from "../../../tokenizer";
+
+export default [
+	{
+		type: "text",
+		text: "Hello ",
+	},
+	{
+		type: "text",
+		text: "this ",
+	},
+	{
+		type: "text",
+		text: "is ",
+	},
+	{
+		type: "text",
+		text: "just some example test",
+	},
+] as Token[];
