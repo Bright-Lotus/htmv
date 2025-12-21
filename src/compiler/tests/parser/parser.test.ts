a6a6528a78ef7053bfcca4902256cb69d6938001
fabrisdev <ezepuntocom@hotmail.com>
2025-12-21 19:44:05 -0300

    Copied from renderer.test.ts

---
+++ src/compiler/tests/parser/parser.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from "bun:test";
+import fs from "node:fs/promises";
+import path from "node:path";
+import { render } from "../../renderer";
+
+const inputFilesPath = path.resolve(import.meta.dir, "input");
+const outputFilesPath = path.resolve(import.meta.dir, "output");
+const inputFilesNames = await fs.readdir(inputFilesPath);
+
+describe("Renderer tests", () => {
+  inputFilesNames.forEach((inputFileName) => {
+    const name = inputFileName.slice(0, -".ts".length);
+    test(name, async () => {
+      const input = (await import(path.resolve(inputFilesPath, inputFileName)))
+        .default;
+      const output = render(input.root, input.context);
+      const expectedOutput = await Bun.file(
+        path.resolve(outputFilesPath, `${name}.html`),
+      ).text();
+      expect(output).toEqual(expectedOutput);
+    });
+  });
+});
