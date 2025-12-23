4fddb9fd2e55bbf657293a2edd6a7d3c4b0a6a38
fabrisdev <ezepuntocom@hotmail.com>
2025-12-23 01:32:58 -0300

    Removed unnecessary return type

--- src/response.ts
+++ src/response.ts
@@ -39,17 +39,11 @@ export function httpResponse(response: HttpResponse) {
 	return response;
 }
 
-export function badRequest(
-	body?: string | object,
-	headers?: Headers,
-): HttpResponse {
+export function badRequest(body?: string | object, headers?: Headers) {
 	return requestHelper(400, body, headers);
 }
 
-export function created(
-	body?: string | object,
-	headers?: Headers,
-): HttpResponse {
+export function created(body?: string | object, headers?: Headers) {
 	return requestHelper(201, body, headers);
 }
 
