def12473d7927cf520ef2732bcb4cb2c26079d9b
fabrisdev <ezepuntocom@hotmail.com>
2025-12-25 21:56:14 -0300

    Added helper for reading dirs

--- src/views-registry.ts
+++ src/views-registry.ts
@@ -1,5 +1,27 @@
+import { viewsPath } from "./views";
+import fs from "node:fs/promises";
+import path from "node:path";
+
 export const viewRegistry: Record<string, string> = {};
 
 export function addToViewRegistry(name: string, code: string) {
 	viewRegistry[name] = code;
+}
+
+async function deepReadDir(dir: string): Promise<string[]> {
+	const entries = await fs.readdir(dir, { withFileTypes: true });
+
+	const files: string[] = [];
+
+	for (const entry of entries) {
+		const fullPath = path.join(dir, entry.name);
+
+		if (entry.isDirectory()) {
+			files.push(...(await deepReadDir(fullPath)));
+		} else if (entry.isFile()) {
+			files.push(fullPath);
+		}
+	}
+
+	return files;
 }
