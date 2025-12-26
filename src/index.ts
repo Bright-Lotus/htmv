5e7e854739ee59357821aeb2a24d270ce39cde18
fabrisdev <ezepuntocom@hotmail.com>
2025-12-25 22:00:28 -0300

    register views on htmv start

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { createApp } from "./app";
 import { registerRoutes } from "./routing/routing";
 import type { Paths } from "./types";
 import { setViewsPath } from "./views";
+import { registerViews } from "./views-registry";
 
 export type { RouteParams } from "./types";
 export { view } from "./views";
 
 export async function setup(paths: Paths) {
 	setViewsPath(paths.views);
+	await registerViews();
 	const app = createApp(paths.public);
 	await registerRoutes(app, paths.routes);
 	app.listen(paths.port);
